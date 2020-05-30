09207afdf728534fcf2a82d48a044c274e380261
Nathan Dockens <Nathan@nathans-mbp.attlocal.net>
2020-05-29 20:30:46 -0500

    Added card component

--- App.js
+++ App.js
@@ -2,7 +2,31 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
+import Card from "./app/components/Card";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return (
+    <View style={{ flex: 1, backgroundColor: "#fff" }}>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "#f8f4f4",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: 25,
+        }}
+      >
+        <Card
+          title="Red jacket for sale!"
+          subTitle="$100"
+          image={require("./app/assets/jacket.jpg")}
+        />
+      </View>
+      <View
+        style={{
+          flex: 1,
+        }}
+      />
+    </View>
+  );
 }
---
+++ app/components/Card.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { View, StyleSheet, Text, Image } from "react-native";
+
+function Card({ title, subTitle, image }) {
+  return (
+    <View style={styles.card}>
+      <Image style={styles.image} source={image} />
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.subTitle}>{subTitle}</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    height: 300,
+    width: "100%",
+    backgroundColor: "#fff",
+    borderRadius: 25,
+  },
+  image: {
+    flex: 1,
+    width: "100%",
+    resizeMode: "cover",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+  },
+  subTitle: {
+    fontSize: 16,
+    color: "turquoise",
+    paddingTop: 10,
+  },
+  textContainer: {
+    flex: 0.5,
+    paddingHorizontal: 20,
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 16,
+  },
+});
+
+export default Card;
