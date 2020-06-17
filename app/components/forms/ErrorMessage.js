df52ad70933fa9200717bafff860e68e8b68a1fb
Nathan Dockens <Nathan@nathans-mbp.attlocal.net>
2020-06-16 20:01:57 -0500

    Reorganize component files

--- app/components/forms/ErrorMessage.js
+++ app/components/forms/ErrorMessage.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { StyleSheet } from "react-native";
 
-import AppText from "./AppText";
-import defaultStyles from "../config/styles";
+import AppText from "../AppText";
+import defaultStyles from "../../config/styles";
 
 function ErrorMessage({ error, visible }) {
   if (!visible || !error) return null;
