31a6415da1a540d8c9dbd34d30d07003fa98c892
Nathan Dockens <Nathan@nathans-mbp.attlocal.net>
2020-06-17 19:20:26 -0500

    Add register screen

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import ListingsScreen from "./app/screens/ListingsScreen";
 import AppTextInput from "./app/components/AppTextInput";
 import AppPicker from "./app/components/AppPicker";
 import LoginScreen from "./app/screens/LoginScreen";
+import RegisterScreen from "./app/screens/RegisterScreen";
 
 const categories = [
   { label: "Furniture", value: 1 },
@@ -24,5 +25,5 @@ const categories = [
 export default function App() {
   const [category, setCategory] = useState(categories[0]);
 
-  return <LoginScreen></LoginScreen>;
+  return <RegisterScreen></RegisterScreen>;
 }
---
+++ app/screens/RegisterScreen.js
@@ -0,0 +1,65 @@
+import React from "react";
+import * as Yup from "yup";
+
+import { StyleSheet } from "react-native";
+
+import Screen from "../components/Screen";
+import { AppForm, AppFormField, SubmitButton } from "../components/forms";
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required().label("Name"),
+  email: Yup.string().required().email().label("Email"),
+  password: Yup.string().required().min(4).label("Password"),
+});
+
+function RegisterScreen(props) {
+  return (
+    <Screen style={styles.container}>
+      <AppForm
+        initialValues={{ name: "", email: "", password: "" }}
+        onSubmit={(values) => console.log(values)}
+        validationSchema={validationSchema}
+      >
+        <AppFormField
+          autoCapitalize="words"
+          autoCorrect={false}
+          icon="account"
+          keyboardType="default"
+          name="name"
+          placeholder="Name"
+          textContentType="name"
+        />
+        <AppFormField
+          autoCapitalize="none"
+          autoCorrect={false}
+          icon="email"
+          keyboardType="email-address"
+          name="email"
+          placeholder="Email"
+          textContentType="emailAddress"
+        />
+        <AppFormField
+          autoCapitalize="none"
+          autoCorrect={false}
+          icon="lock"
+          name="password"
+          placeholder="Password"
+          secureTextEntry
+          textContentType="password"
+        />
+        <SubmitButton style={styles.registerButton} title="Register" />
+      </AppForm>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  registerButton: {
+    marginTop: 10,
+  },
+});
+
+export default RegisterScreen;
