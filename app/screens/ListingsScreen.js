6d0309b64bae202122fd58c4b3b1826aba08b4cd
Nathan Dockens <Nathan@nathans-mbp.attlocal.net>
2020-07-29 20:20:22 -0500

    Add error handling in listings page

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,29 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, FlatList } from "react-native";
 
+import AppText from "../components/Text";
 import Screen from "../components/Screen";
 import Card from "../components/Card";
 import colors from "../config/colors";
 import listingsApi from "../api/listings";
-
-const initialListings = [
-  {
-    id: 1,
-    title: "Red jacket for sale",
-    price: "100",
-    image: require("../assets/jacket.jpg"),
-  },
-  {
-    id: 2,
-    title: "Couch in great condition",
-    price: "1000",
-    image: require("../assets/couch.jpg"),
-  },
-];
+import Button from "../components/Button";
 
 function ListingsScreen({ navigation }) {
   const [listings, setListings] = useState([]);
   const [refreshing, setRefreshing] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     loadListings();
@@ -31,11 +19,23 @@ function ListingsScreen({ navigation }) {
 
   const loadListings = async () => {
     const response = await listingsApi.getListings();
+
+    if (!response.ok) return setError(true);
+
+    setError(false);
     setListings(response.data);
   };
 
   return (
     <Screen style={styles.screen}>
+      {error && (
+        <>
+          <AppText style={styles.errorMessage}>
+            There was a problem loading the data
+          </AppText>
+          <Button title="Retry" onPress={loadListings} />
+        </>
+      )}
       <FlatList
         data={listings}
         keyExtractor={(listing) => listing.id.toString()}
@@ -67,6 +67,10 @@ const styles = StyleSheet.create({
   screen: {
     padding: 20,
     backgroundColor: colors.light,
+  },
+  errorMessage: {
+    margin: 15,
+    textAlign: "center",
   },
 });
 
