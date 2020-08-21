c4532b3e9d33d6d9f74b2533ebcf67e14100e427
Nathan Dockens <Nathan@nathans-mbp.attlocal.net>
2020-08-21 16:19:06 -0500

    Implement caching in all API requests

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { StyleSheet, View, Image } from "react-native";
+import { StyleSheet, View } from "react-native";
 import Text from "../components/Text";
 import ListItem from "../components/ListItem";
+import { Image } from "react-native-expo-image-cache";
 
 import colors from "../config/colors";
 
@@ -10,7 +11,12 @@ function ListingDetailsScreen({ route }) {
 
   return (
     <View>
-      <Image style={styles.image} source={listing.image} />
+      <Image
+        style={styles.image}
+        preview={{ uri: listing.images[0].thumbnailUrl }}
+        uri={listing.images[0].url}
+        tint="light"
+      />
       <View style={styles.detailsContainer}>
         <Text style={styles.title}>{listing.title}</Text>
         <Text style={styles.price}>${listing.price}</Text>
--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -40,6 +40,7 @@ function ListingsScreen({ navigation }) {
             subTitle={"$" + item.price}
             imageUrl={item.images[0].url}
             onPress={() => navigation.navigate("ListingDetails", item)}
+            thumbnailUrl={item.images[0].thumbnailUrl}
           />
         )}
         refreshing={refreshing}
