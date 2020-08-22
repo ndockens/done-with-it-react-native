13f243c24b6f9fc8ab7465f7ab44b2c8216fca14
Nathan Dockens <Nathan@nathans-mbp.attlocal.net>
2020-08-22 14:26:40 -0500

    Refactor auth token retrieval from storage

--- App.js
+++ App.js
@@ -1,56 +1,26 @@
-import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
-
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import Card from "./app/components/Card";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
-import ListItem from "./app/components/ListItem";
-import Icon from "./app/components/Icon";
-import colors from "./app/config/colors";
-import Screen from "./app/components/Screen";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingsScreen from "./app/screens/ListingsScreen";
-import TextInput from "./app/components/TextInput";
-import Picker from "./app/components/Picker";
-import LoginScreen from "./app/screens/LoginScreen";
-import RegisterScreen from "./app/screens/RegisterScreen";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
-import ImageInput from "./app/components/ImageInput";
-import ImageInputList from "./app/components/ImageInputList";
-import {
-  Form,
-  FormField,
-  FormImagePicker,
-  FormPicker,
-  SubmitButton,
-} from "./app/components/forms";
-import * as Yup from "yup";
-import AuthNavigator from "./app/navigation/AuthNavigator";
+import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import navigationTheme from "./app/navigation/navigationTheme";
+import { AppLoading } from "expo";
+
+import AuthNavigator from "./app/navigation/AuthNavigator";
 import AppNavigator from "./app/navigation/AppNavigator";
+import navigationTheme from "./app/navigation/navigationTheme";
 import AuthContext from "./app/auth/context";
 import authStorage from "./app/auth/storage";
-import jwtDecode from "jwt-decode";
-import { AppLoading } from "expo";
 
 export default function App() {
   const [user, setUser] = useState();
   const [isReady, setIsReady] = useState(false);
 
-  const restoreToken = async () => {
-    const token = await authStorage.getToken();
+  const restoreUser = async () => {
+    const user = await authStorage.getUser();
 
-    if (!token) return;
-
-    setUser(jwtDecode(token));
+    if (user) setUser(user);
   };
 
   if (!isReady)
     return (
-      <AppLoading startAsync={restoreToken} onFinish={() => setIsReady(true)} />
+      <AppLoading startAsync={restoreUser} onFinish={() => setIsReady(true)} />
     );
 
   return (
