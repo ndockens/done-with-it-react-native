19010a6263541b478628f52f1b73fecb6eb23023
Nathan Dockens <Nathan@nathans-mbp.attlocal.net>
2020-08-22 15:35:37 -0500

    Implement register functionality

--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import * as Yup from "yup";
+
+import usersApi from "../api/users";
+import authApi from "../api/auth";
+import useAuth from "../auth/useAuth";
 
 import { StyleSheet } from "react-native";
 
 import Screen from "../components/Screen";
-import { Form, FormField, SubmitButton } from "../components/forms";
+import {
+  ErrorMessage,
+  Form,
+  FormField,
+  SubmitButton,
+} from "../components/forms";
 
 const validationSchema = Yup.object().shape({
   name: Yup.string().required().label("Name"),
@@ -13,13 +22,39 @@ const validationSchema = Yup.object().shape({
 });
 
 function RegisterScreen(props) {
+  const [error, setError] = useState();
+  const auth = useAuth();
+
+  const handleSubmit = async (userInfo) => {
+    const response = await usersApi.register(userInfo);
+
+    if (!response.ok) {
+      if (response.data) {
+        setError(response.data.error);
+      } else {
+        setError("An unexpected error has occurred.");
+        console.log(result);
+      }
+      return;
+    }
+
+    logIn(userInfo.email, userInfo.password);
+  };
+
+  const logIn = async (email, password) => {
+    const response = await authApi.login(email, password);
+    if (!response.ok) return;
+    auth.logIn(response.data);
+  };
+
   return (
     <Screen style={styles.container}>
       <Form
         initialValues={{ name: "", email: "", password: "" }}
-        onSubmit={(values) => console.log(values)}
+        onSubmit={handleSubmit}
         validationSchema={validationSchema}
       >
+        <ErrorMessage error={error} visible={error} />
         <FormField
           autoCapitalize="words"
           autoCorrect={false}
