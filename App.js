89a8f3b0853e7bce21cb50d8e41cd6828e6bab29
Nathan Dockens <Nathan@nathans-mbp.attlocal.net>
2020-08-22 14:18:09 -0500

    Add custom splash screen

--- App.js
+++ App.js
@@ -34,9 +34,11 @@ import AppNavigator from "./app/navigation/AppNavigator";
 import AuthContext from "./app/auth/context";
 import authStorage from "./app/auth/storage";
 import jwtDecode from "jwt-decode";
+import { AppLoading } from "expo";
 
 export default function App() {
   const [user, setUser] = useState();
+  const [isReady, setIsReady] = useState(false);
 
   const restoreToken = async () => {
     const token = await authStorage.getToken();
@@ -46,9 +48,10 @@ export default function App() {
     setUser(jwtDecode(token));
   };
 
-  useEffect(() => {
-    restoreToken();
-  }, []);
+  if (!isReady)
+    return (
+      <AppLoading startAsync={restoreToken} onFinish={() => setIsReady(true)} />
+    );
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
