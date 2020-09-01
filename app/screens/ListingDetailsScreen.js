ed7bc3694bd57b870ea89ae4d2e6815154ca3cd1
Nathan Dockens <Nathan@nathans-mbp.attlocal.net>
2020-08-31 21:51:46 -0500

    Add form to contact seller

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,38 +1,98 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
-import Text from "../components/Text";
-import ListItem from "../components/ListItem";
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  StyleSheet,
+  View,
+  ScrollView,
+} from "react-native";
 import { Image } from "react-native-expo-image-cache";
+import * as Yup from "yup";
+import { Notifications } from "expo";
+
+import { Form, FormField, SubmitButton } from "../components/forms";
+import ListItem from "../components/ListItem";
+import Text from "../components/Text";
 
 import colors from "../config/colors";
+import messagesApi from "../api/messages";
 
 function ListingDetailsScreen({ route }) {
   const listing = route.params;
 
+  const validationSchema = Yup.object().shape({
+    message: Yup.string().required().min(1).label("Message"),
+  });
+
+  const handleSendMessage = async ({ message }, { resetForm }) => {
+    Keyboard.dismiss();
+    const response = await messagesApi.send(message, listing.id);
+
+    if (!response.ok) {
+      alert("There was a problem sending your message.");
+      return;
+    }
+
+    resetForm();
+
+    Notifications.presentLocalNotificationAsync({
+      title: "Success",
+      body: "Your message has been sent!",
+      ios: {
+        _displayInForeground: true,
+      },
+    });
+  };
+
   return (
-    <View>
-      <Image
-        style={styles.image}
-        preview={{ uri: listing.images[0].thumbnailUrl }}
-        uri={listing.images[0].url}
-        tint="light"
-      />
-      <View style={styles.detailsContainer}>
-        <Text style={styles.title}>{listing.title}</Text>
-        <Text style={styles.price}>${listing.price}</Text>
-      </View>
-      <View style={styles.userContainer}>
-        <ListItem
-          title="Mosh Hamedani"
-          subTitle="5 Listings"
-          image={require("../assets/mosh.jpg")}
+    <KeyboardAvoidingView
+      behavior="position"
+      keyboardVerticalOffset={Platform.OS === "ios" ? 0 : 100}
+    >
+      <ScrollView>
+        <Image
+          style={styles.image}
+          preview={{ uri: listing.images[0].thumbnailUrl }}
+          uri={listing.images[0].url}
+          tint="light"
         />
-      </View>
-    </View>
+        <View style={styles.detailsContainer}>
+          <Text style={styles.title}>{listing.title}</Text>
+          <Text style={styles.price}>${listing.price}</Text>
+        </View>
+        <View style={styles.userContainer}>
+          <ListItem
+            title="Mosh Hamedani"
+            subTitle="5 Listings"
+            image={require("../assets/mosh.jpg")}
+          />
+        </View>
+        <View style={styles.contactForm}>
+          <Form
+            initialValues={{ message: "" }}
+            onSubmit={handleSendMessage}
+            validationSchema={validationSchema}
+          >
+            <FormField
+              maxLength={255}
+              name="message"
+              placeholder="Message..."
+            />
+            <SubmitButton style={styles.contactButton} title="Contact Seller" />
+          </Form>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
+  contactForm: {
+    paddingHorizontal: 20,
+  },
+  contactButton: {
+    marginTop: 10,
+  },
   detailsContainer: {
     padding: 20,
   },
